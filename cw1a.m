16e50d884cd6712900a3e2ba7b75b2c22738f09b
Xia Hua <34142126+ravelhua@users.noreply.github.com>
2019-11-23 15:41:45 +0000

    Updates

---
+++ cw1a.m
@@ -0,0 +1,20 @@
+load('cw1a.mat');
+xs = linspace(-4, 4, 1000)';
+
+meanfunc = [];
+
+covfunc = @covSEiso;
+
+likfunc = @likGauss;
+
+hyp = struct('mean', [], 'cov', [-1 0], 'lik', 0);
+
+hyp2 = minimize(hyp, @gp, -100, @infGaussLik, meanfunc, covfunc, likfunc, x, y);
+
+% make predictions 
+[mu s2] = gp(hyp2, @infGaussLik, meanfunc, covfunc, likfunc, x, y, xs);
+
+% plot the predictive mean
+f = [mu+2*sqrt(s2); flipdim(mu-2*sqrt(s2),1)];
+fill([xs; flipdim(xs,1)], f, [5 5.5 8]/8, 'LineStyle', 'none');
+hold on; plot(xs, mu,'r'); plot(x, y, 'bx')
